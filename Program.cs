edcc5c4ce7d6cf1fef50ab88dee9860b9f4f338d
Davi Martins <davi_martins2009outlook.com>
2024-03-04 17:19:28 -0300

    Pagina de listagem

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoMVC.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AgendaContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
--- obj/Debug/net8.0/ProjetoMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjetoMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e31757f21967a8bb2fbd5c470c2b2ed8b90289")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
